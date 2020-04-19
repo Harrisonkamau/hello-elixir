b1cb0765a0acca899aed865b5508ce1dad99923d
Harrisonkamau <kamauharrison87@gmail.com>
2020-04-19 17:04:08 +0300

    Convert List Enum methods into one-liners

--- modules/list_enum.exs
+++ modules/list_enum.exs
@@ -1,14 +1,8 @@
 defmodule List.Enum do
   import Logger, only: [info: 1]
 
-  def first([]) do
-    nil
-  end
-
-  def first([head | tail]) do
-    # return the first item in the list using the `head` (hd) buil-in list method
-    info "First item in the list is: '#{head}'"
-  end
+  def first([]), do: nil
+  def first([head | _]), do: info "First item in the list is: '#{head}'"
 end
 
 todos = ['Take Breakfast', 'Workout', 'Learn Elixir', 'Write some modules']
