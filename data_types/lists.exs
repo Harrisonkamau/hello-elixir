8c7a29521a558fbaf2fac222f2aca91cf77f059e
Harrisonkamau <kamauharrison87@gmail.com>
2020-04-19 21:12:12 +0300

    Functions as first-class citizens - pass them as arguments

---
+++ data_types/lists.exs
@@ -0,0 +1,21 @@
+defmodule MyList do
+  import Enum
+
+  def map(list) do
+    # return a list with squared items
+    squares = map(list, &square/1)
+    print(squares, "Mapped list")
+  end
+
+  # private function
+  defp square(num) do
+    num * num
+  end
+
+  defp print(list, label) do
+    IO.inspect(list, label: label)
+  end
+end
+
+
+MyList.map([1, 2, 3, 4])
