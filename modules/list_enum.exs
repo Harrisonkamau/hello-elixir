ee9388df0c15e0eb43cfe3ed69f5cfe1545382e8
Harrisonkamau <kamauharrison87@gmail.com>
2020-04-19 17:02:29 +0300

    Handle empty lists while retrieving first item

--- modules/list_enum.exs
+++ modules/list_enum.exs
@@ -1,9 +1,13 @@
 defmodule List.Enum do
   import Logger, only: [info: 1]
 
-  def first(list) do
+  def first([]) do
+    nil
+  end
+
+  def first([head | tail]) do
     # return the first item in the list using the `head` (hd) buil-in list method
-    info "First item in the list is: '#{hd(list)}'"
+    info "First item in the list is: '#{head}'"
   end
 end
 
