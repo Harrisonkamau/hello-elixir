f1ebdeff752ef2a35f7003de39d04f34e70ccecf
Harrisonkamau <kamauharrison87@gmail.com>
2020-04-19 14:07:41 +0300

    Basic data types & use of Logger module across the dir

---
+++ data_types/numbers.exs
@@ -0,0 +1,6 @@
+import Logger, only: [info: 1]
+
+number = 2
+
+# todos = ['Take Breakfast', 'Workout', 'Learn Elixir', 'Write some modules']
+info "NUMBER: #{number}"
---
+++ data_types/strings.exs
@@ -0,0 +1,7 @@
+import Logger, only: [info: 1]
+
+first_name = "Harrison"
+last_name = "Kamau"
+
+full_name = "#{first_name} #{last_name}"
+info full_name
